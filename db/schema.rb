98e83666a2c7b7601f79e5a717c53b27308f4920
Faye.H <yang5chen12hui@gmail.com>
2021-01-11 19:41:03 +0900

    マイグレーションファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_050819) do
+ActiveRecord::Schema.define(version: 2021_01_11_104010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_01_08_050819) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posting_pages", "users"
