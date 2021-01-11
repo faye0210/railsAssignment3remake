66f29faca8fb42e3ea12313988354e79a673bb57
Faye.H <yang5chen12hui@gmail.com>
2021-01-11 19:22:05 +0900

    マイグレーションファイルの追加

--- db/migrate/20210108050819_create_posting_pages.rb
+++ db/migrate/20210108050819_create_posting_pages.rb
@@ -2,7 +2,8 @@ class CreatePostingPages < ActiveRecord::Migration[5.2]
   def change
     create_table :posting_pages do |t|
       t.text :content
-
+      t.string :image
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
